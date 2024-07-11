ded2d4d2252768b4643ee966917954ca0f564eb9
Rakibul583 <islammdrakibul583@gmail.com>
2024-07-11 21:36:33 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Users></Users>
       </header>
     </div>
   );
 }
 
+
+function Users() {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(data => setUsers(data));
+  }
+  )
+
+
+  return (
+    <div>
+      <h1>Dynamic users:{users.length}</h1>
+      <h1>List:</h1>
+      <ul>
+        {
+          users.map(user =>
+            <li>{user.id}</li>
+          )
+        }
+        <br /><br />
+        {
+          users.map(user =>
+            <li>{user.name}</li>
+          )
+        }
+        <br /><br />
+        {
+          users.map(user =>
+            <li>{user.email}</li>
+          )
+        }
+      </ul>
+    </div>
+  )
+}
 export default App;
